3cda5ca773d9ba13e53aa5547c4c22b5b8fc7786
Chloe Arkwright <chloe@ellie.moi>
2026-01-07 18:43:47 +0000

    Try and fix it part 2?

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs =
     inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      imports = [ "./modules/flake" ];
+      imports = [ ./modules/flake ];
     };
 
   inputs = {
