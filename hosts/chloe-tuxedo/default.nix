0e3259fdf8b23033dde4a4f37431905c25d9f27a
Chloe Arkwright <chloe@ellie.moi>
2026-01-07 18:34:24 +0000

    Enable flakes

--- hosts/chloe-tuxedo/default.nix
+++ hosts/chloe-tuxedo/default.nix
@@ -137,4 +137,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 }
