9e23e531d5987bc4401cb5aa20034a6731263830
Chloe Arkwright <chloe@ellie.moi>
2026-01-07 18:24:52 +0000

    Initial flake, hopefully works.

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Chloe's personal dotfiles/NixOS config";
+
+  outputs =
+    inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } { imports = [ ./modules/flake ]; };
+
+  inputs = {
+    # https://deer.social/profile/did:plc:mojgntlezho4qt7uvcfkdndg/post/3loogwsoqok2w
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+
+    # manage userspace with nix
+    home-manager = {
+      type = "github";
+      owner = "nix-community";
+      repo = "home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # bring all the mess together with flake-parts
+    flake-parts = {
+      type = "github";
+      owner = "hercules-ci";
+      repo = "flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # easily manage our hosts
+    easy-hosts = {
+      type = "github";
+      owner = "tgirlcloud";
+      repo = "easy-hosts";
+    };
+
+    homebrew = {
+      type = "github";
+      owner = "zhaofengli";
+      repo = "nix-homebrew";
+    };
+  };
+}
