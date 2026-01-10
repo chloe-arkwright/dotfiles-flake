d94cb22c2896c207325622333fc18fb4f172e68b
Chloe Arkwright <chloe@ellie.moi>
2026-01-10 22:45:10 +0000

    this then?

--- hosts/chloe-tuxedo/hardware-configuration.nix
+++ hosts/chloe-tuxedo/hardware-configuration.nix
@@ -52,7 +52,7 @@
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-    services.way-displays = {
+    home-manager.services.way-displays = {
         enable = true;
 
         settings = {
