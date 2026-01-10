a49df4babc1f142908716da6f12ff986c2069f91
Chloe Arkwright <chloe@ellie.moi>
2026-01-10 22:37:10 +0000

    How about this?

--- hosts/chloe-tuxedo/hardware-configuration.nix
+++ hosts/chloe-tuxedo/hardware-configuration.nix
@@ -52,33 +52,36 @@
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-    services.way-displays.enable = true;
-    services.way_displays.settings = {
-        ORDER = [
-            "HDMI-A-1"
-            "eDP-1"
-        ];
-        SCALING = false;
-        MODE = [
-            {
-                NAME_DESC = "HDMI-A-1";
-                WIDTH = 1920;
-                HEIGHT = 1080;
-                HZ = 144;
-            }
+    home-manager.services.way_displays = {
+        enable = true;
 
-            {
-                NAME_DESC = "eDP-1";
-                WIDTH = 2560;
-                HEIGHT = 1600;
-                HZ = 240;
-            }
-        ];
-        SCALE = [
-            {
-                NAME_DESC = "eDP-1";
-                SCALE = 1.25;
-            }
-        ];
+        settings = {
+            ORDER = [
+                "HDMI-A-1"
+                "eDP-1"
+            ];
+            SCALING = false;
+            MODE = [
+                {
+                    NAME_DESC = "HDMI-A-1";
+                    WIDTH = 1920;
+                    HEIGHT = 1080;
+                    HZ = 144;
+                }
+
+                {
+                    NAME_DESC = "eDP-1";
+                    WIDTH = 2560;
+                    HEIGHT = 1600;
+                    HZ = 240;
+                }
+            ];
+            SCALE = [
+                {
+                    NAME_DESC = "eDP-1";
+                    SCALE = 1.25;
+                }
+            ];
+        };
     };
 }
